8f6413175c7563cfd8fb31b70b52fea91a4b5869
Aashish Ananthanarayan <aashish.2704@tamu.edu>
2020-11-16 21:32:10 -0600

    Added algorithm changes

--- apicall.py
+++ apicall.py
@@ -1,6 +1,8 @@
 import requests
 import time
-
+from sklearn.metrics import pairwise_kernels
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 edamam_response = requests.get("https://api.edamam.com/search?app_id=6e3a1c55&app_key=979f4ea3d3c1014ffa5de338accf640b")
 print(edamam_response)      # 200 – OK. The request was successful.
@@ -20,9 +22,9 @@ allergy_dict = {'peanut': 'peanut-free', 'peanuts': 'peanut-free', 'tree nut': '
 print("Welcome to the Allergy App!")
 
 print("What would you like to cook?")
-q_input = input()
+q_input = "Borscht"
 print("What is your food allergy?")
-health_input = input()
+health_input = "Peanut"
 
 health_arg = ""
 allergen_found = False
@@ -37,5 +39,47 @@ if allergen_found:
 else:
     link = url + q + q_input + "&" + edamam_id + "&" + edamam_key + "&" + q + q_input + "&" + "excluded" + "=" + health_input
 
-print(link)
-print(requests.get(link).json())
+
+
+x = requests.get(link).json()
+
+
+listing = []
+for i in range(0, int(x['to'])):
+    listing.append(x['hits'][i]['recipe']['ingredientLines'])
+
+secondlist = []
+
+
+for smalllist in listing:
+    xlist = " ".join(smalllist)
+    secondlist.append(xlist)
+
+
+tfidf_vectorizer=TfidfVectorizer()
+
+X_train_counts = tfidf_vectorizer.fit_transform(secondlist)
+similarities = cosine_similarity(X_train_counts) 
+
+
+final_list = []
+
+list0 = similarities[0].tolist()
+
+list1 = similarities[0].tolist()
+
+list1.sort(reverse = True)
+
+list3 = []
+
+for i in range(0,6):
+    list3.append(list1[i])
+
+indices = []
+
+for element in list3:
+    index = list0.index(element)
+    indices.append(index)
+
+print(indices)
+
