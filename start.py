f3f5f5202b31aa3275e5d43ac0a0c2b793e6fa1d
Aashish Ananthanarayan <aashish.2704@tamu.edu>
2020-11-18 18:07:57 -0600

    Added changes to UI and integrated :)

--- start.py
+++ start.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template, session, request, jsonify, Response, flash, redirect, url_for
+import requests
+import time
+from sklearn.metrics import pairwise_kernels
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 app = Flask(__name__)
 
 
@@ -9,6 +15,91 @@ def hello_world():
 @app.route('/allergies')
 def allergies():
     return render_template('allergies.html')
+
+@app.route('/allergies_response', methods = ['POST'])
+def allergies_response():
+    allergy_name = request.form['One']
+    recipe_name = request.form['Two']
+    edamam_response = requests.get("https://api.edamam.com/search?app_id=6e3a1c55&app_key=979f4ea3d3c1014ffa5de338accf640b")
+
+    url = 'https://api.edamam.com/search?'
+    edamam_id = 'app_id=6e3a1c55'
+    edamam_key = 'app_key=979f4ea3d3c1014ffa5de338accf640b'
+    q = 'q='
+    health = 'health='
+
+    allergy_dict = {'peanut': 'peanut-free', 'peanuts': 'peanut-free', 'tree nut': 'tree-nut-free',
+                    'dairy': 'dairy-free', 'milk' : 'dairy-free', 'alcohol': 'alcohol-free',
+                    'celery' : 'celery-free', 'eggs' : 'egg-free', 'fish' : 'fish-free',
+                    'gluten' : 'gluten-free', 'mustard' : 'mustard-free', 'pork' : 'pork-free',
+                    'sesame' : 'sesame-free', 'shellfish' : 'shellfish-free', 'soy' : 'soy-free',
+                    'tree nuts' : 'tree-nut-free', 'wheat' : 'wheat-free'}
+
+    health_arg = ""
+    allergen_found = False
+    for key,val in allergy_dict.items():
+        if(allergy_name == key):
+            health_arg = val
+            allergen_found = True
+
+    link = ""
+    if allergen_found:
+        link = url + q + recipe_name + "&" + edamam_id + "&" + edamam_key + "&" + q + recipe_name + "&" + health + health_arg
+    else:
+        link = url + q + recipe_name  + "&" + edamam_id + "&" + edamam_key + "&" + q + recipe_name + "&" + "excluded" + "=" + allergy_name
+
+    #print(link)
+    x = requests.get(link).json()
+
+    listing = []
+    for i in range(0, int(x['to'])):
+        listing.append(x['hits'][i]['recipe']['ingredientLines'])
+
+    secondlist = []
+
+
+    for smalllist in listing:
+        xlist = " ".join(smalllist)
+        secondlist.append(xlist)
+
+
+    tfidf_vectorizer=TfidfVectorizer()
+
+    X_train_counts = tfidf_vectorizer.fit_transform(secondlist)
+    similarities = cosine_similarity(X_train_counts) 
+
+
+    final_list = []
+
+    list0 = similarities[0].tolist()
+
+    list1 = similarities[0].tolist()
+
+    list1.sort(reverse = True)
+
+    list3 = []
+
+    for i in range(0,6):
+        list3.append(list1[i])
+
+    indices = []
+
+    for element in list3:
+        index = list0.index(element)
+        indices.append(index)
+
+    #print(indices)
+
+
+    images = []
+    urls = []
+
+    for i in indices:
+        images.append(x['hits'][i]['recipe']['image'])
+        urls.append(x['hits'][i]['recipe']['url'])
+
+
+    return render_template('output_recipe.html', images = images, urls = urls)
 
 @app.route('/recipes')
 def recipes():
