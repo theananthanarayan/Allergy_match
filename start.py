f36a1dd6a880d67846691c7e452a777c72cea201
Aashish Ananthanarayan <aashish.2704@tamu.edu>
2020-11-18 20:12:37 -0600

    Added changes

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ import time
 from sklearn.metrics import pairwise_kernels
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
+from rank_bm25 import BM25Okapi
 
 app = Flask(__name__)
 
@@ -20,6 +21,8 @@ def allergies():
 def allergies_response():
     allergy_name = request.form['One']
     recipe_name = request.form['Two']
+    ingredient_preferences = request.form['Three']
+    print(ingredient_preferences)
     edamam_response = requests.get("https://api.edamam.com/search?app_id=6e3a1c55&app_key=979f4ea3d3c1014ffa5de338accf640b")
 
     url = 'https://api.edamam.com/search?'
@@ -62,41 +65,68 @@ def allergies_response():
         xlist = " ".join(smalllist)
         secondlist.append(xlist)
 
+    #print(secondlist)
 
-    tfidf_vectorizer=TfidfVectorizer()
+    if ingredient_preferences == "":
 
-    X_train_counts = tfidf_vectorizer.fit_transform(secondlist)
-    similarities = cosine_similarity(X_train_counts) 
+        tfidf_vectorizer=TfidfVectorizer()
 
-
-    final_list = []
-
-    list0 = similarities[0].tolist()
-
-    list1 = similarities[0].tolist()
-
-    list1.sort(reverse = True)
-
-    list3 = []
-
-    for i in range(0,6):
-        list3.append(list1[i])
-
-    indices = []
-
-    for element in list3:
-        index = list0.index(element)
-        indices.append(index)
-
-    #print(indices)
+        X_train_counts = tfidf_vectorizer.fit_transform(secondlist)
+        similarities = cosine_similarity(X_train_counts) 
 
 
-    images = []
-    urls = []
+        final_list = []
 
-    for i in indices:
-        images.append(x['hits'][i]['recipe']['image'])
-        urls.append(x['hits'][i]['recipe']['url'])
+        list0 = similarities[0].tolist()
+
+        list1 = similarities[0].tolist()
+
+        list1.sort(reverse = True)
+
+        list3 = []
+
+        for i in range(0,6):
+            list3.append(list1[i])
+
+        indices = []
+
+        for element in list3:
+            index = list0.index(element)
+            indices.append(index)
+
+        #print(indices)
+
+
+        images = []
+        urls = []
+
+        for i in indices:
+            images.append(x['hits'][i]['recipe']['image'])
+            urls.append(x['hits'][i]['recipe']['url'])
+
+    else:
+        
+        tokenized_secondlist = [doc.split(" ") for doc in secondlist]
+        bm25 = BM25Okapi(tokenized_secondlist)
+        tokenized_query = ingredient_preferences.split(" ")
+        scores = bm25.get_scores(tokenized_query)
+
+        finalized_list = bm25.get_top_n(tokenized_query, secondlist, n=6)
+
+        indices = []
+
+        for element in finalized_list:
+            index = secondlist.index(element)
+            indices.append(index)
+
+        
+        images = []
+        urls = []
+
+        for i in indices:
+            images.append(x['hits'][i]['recipe']['image'])
+            urls.append(x['hits'][i]['recipe']['url'])
+
 
 
     return render_template('output_recipe.html', images = images, urls = urls)
